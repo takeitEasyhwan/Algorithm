cf742b9b30af016134bf20e985dfd01dc5d2250c
Jihwan Lee <wlghks0508@naver.com>
2022-12-29 21:16:15 +0900

    second commit

--- Main_2798.java
+++ Main_2798.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-public class Main {
+public class Main_2798 {
 	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
 		
