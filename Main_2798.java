89fef4151a76edf1c3f6b2f7a092e4e164271af1
Jihwan Lee <wlghks0508@naver.com>
2022-12-29 22:03:39 +0900

    first commit

--- Main_2798.java
+++ Main_2798.java
@@ -8,6 +8,7 @@ public class Main_2798 {
 	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
 		
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine()," ");
 		int n = Integer.parseInt(st.nextToken());
