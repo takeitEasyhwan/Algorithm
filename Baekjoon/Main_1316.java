9f4cac33eb80c6784cf2e3936884c92997824be7
Leejihwan <wlghks0508@naver.com>
2023-01-04 01:05:29 +0900

    Add files via upload

---
+++ Baekjoon/Main_1316.java
@@ -0,0 +1,35 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_1316 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int arr[] = new int['z'-'a'+1];
+		int cnt=0;
+		for(int j=0;j<n;j++) {
+			String word = br.readLine();
+			for(int i=0;i<arr.length;i++)
+				arr[i]=0;
+			for(int i=0;i<word.length();i++) {
+				if(arr[word.charAt(i)-'a']==0)
+					arr[word.charAt(i)-'a']++;
+				else {
+					if(word.charAt(i-1)!=word.charAt(i)) {
+						cnt++;
+						break;
+					}
+						
+				}
+			}
+			
+		}
+		System.out.println(n-cnt);
+	}
+
+}
