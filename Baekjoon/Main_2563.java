726a0a0de829679acd11285524144f98c6f9514d
Leejihwan <wlghks0508@naver.com>
2023-01-05 16:16:03 +0900

    Add files via upload

---
+++ Baekjoon/Main_2563.java
@@ -0,0 +1,35 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_2563 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int arr[][] = new int [100][100];
+		int area=0;
+		while(n-->0) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			for(int i=x;i<x+10;i++) {
+				for(int j=y;j<y+10;j++) {
+					arr[i][j]++;
+				}
+			}
+		}
+		for(int i=0;i<100;i++) {
+			for(int j=0;j<100;j++) {
+				if(arr[i][j]>=1)
+					area++;
+			}
+		}
+		System.out.println(area);
+	}
+
+}
