8dfe78793123a12da6429041a77a6e7b5a435ac3
Leejihwan <wlghks0508@naver.com>
2023-01-05 17:14:48 +0900

    Add files via upload

---
+++ Baekjoon/Main_1427.java
@@ -0,0 +1,29 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+
+public class Main_1427 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		String n=br.readLine();
+		ArrayList<Integer> list = new ArrayList<>();
+		for(int i=0;i<n.length();i++) {
+			list.add(Character.getNumericValue(n.charAt(i)));
+		}
+		Collections.sort(list , Collections.reverseOrder());
+		Iterator<Integer> iter = list.iterator();
+		while(iter.hasNext()) {
+			sb.append(iter.next());
+		}
+		System.out.println(sb);
+	}
+
+}
