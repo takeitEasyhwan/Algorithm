390ce74f8d047dd55cc4c0f1fcedfc621d8d492b
Leejihwan <wlghks0508@naver.com>
2023-01-05 17:04:12 +0900

    Add files via upload

--- Baekjoon/Main_2750.java
+++ Baekjoon/Main_2750.java
@@ -3,21 +3,26 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 public class Main_2750 {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		int n=Integer.parseInt(br.readLine());
-		Integer arr[] = new Integer[n];
+		ArrayList<Integer> list = new ArrayList<>();
 		while(n-->0) {
-			arr[n]=Integer.parseInt(br.readLine());
+			list.add(Integer.parseInt(br.readLine()));
 		}
-		Arrays.sort(arr);
-		for(int i=0;i<arr.length;i++) {
-			System.out.println(arr[i]);
+		Collections.sort(list);
+		Iterator iter = list.iterator();
+		while(iter.hasNext()) {
+			sb.append(iter.next()).append("\n");
 		}
+		System.out.println(sb);
 	}
 
 }
