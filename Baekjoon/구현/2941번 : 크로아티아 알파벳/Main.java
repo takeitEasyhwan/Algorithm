71b3ff2b480c77286bd78e36cecbdb9bd17f964b
Leejihwan <wlghks0508@naver.com>
2023-01-09 20:23:58 +0900

    Update Main.java

--- Baekjoon/구현/2941번 : 크로아티아 알파벳/Main.java
+++ Baekjoon/구현/2941번 : 크로아티아 알파벳/Main.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main_2941 {
+public class Main {
 	//2941
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
