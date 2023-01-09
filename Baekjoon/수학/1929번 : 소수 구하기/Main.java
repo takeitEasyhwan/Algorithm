7a41572114904a755fb62818c2ae65d55af19ee2
Leejihwan <wlghks0508@naver.com>
2023-01-09 20:23:16 +0900

    Update Main.java

--- Baekjoon/수학/1929번 : 소수 구하기/Main.java
+++ Baekjoon/수학/1929번 : 소수 구하기/Main.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 
-public class Main_1929 {
+public class Main {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
