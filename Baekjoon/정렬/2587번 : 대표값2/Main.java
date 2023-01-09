89ef593c5bbdf7958da4c124d5ffdecf7478e2eb
Leejihwan <wlghks0508@naver.com>
2023-01-09 20:09:38 +0900

    Update and rename Main_2587.java to Main.java

--- Baekjoon/정렬/2587번 : 대표값2/Main.java
+++ Baekjoon/정렬/2587번 : 대표값2/Main.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-public class Main_2587 {
+public class Main {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
