c341f4604e23b04cb267d4f65aa59d9114a33d0b
Leejihwan <wlghks0508@naver.com>
2023-01-09 20:23:31 +0900

    Update Main.java

--- Baekjoon/수학/2581번 : 소수/Main.java
+++ Baekjoon/수학/2581번 : 소수/Main.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 
-public class Main_2581 {
+public class Main {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
