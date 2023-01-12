90e7b79e45b85d6f918bbeacd370fd3af5775e62
Leejihwan <wlghks0508@naver.com>
2023-01-13 04:14:18 +0900

    Create Main.java

---
+++ Baekjoon/다이나믹 프로그래밍/9655번 : 돌 게임/Main.java
@@ -0,0 +1,17 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		if(N%2==0)System.out.println("CY");
+		else System.out.println("SK");
+	}
+
+}
