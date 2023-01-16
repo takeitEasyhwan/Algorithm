d27a30f15ecf6bc88d32363a81920be9e4e87825
Leejihwan <wlghks0508@naver.com>
2023-01-16 16:54:48 +0900

    Create Main.java

---
+++ Baekjoon/다이나믹 프로그래밍/11726번 : 2×n 타일링/Main.java
@@ -0,0 +1,25 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		BigInteger arr[] = new BigInteger[1001];
+		arr[1]=new BigInteger("1");
+		arr[2]=new BigInteger("2");
+		for(int i=3;i<=n;i++) {
+			arr[i]=arr[i-1].add(arr[i-2]);
+			
+		}
+		System.out.println(arr[n].remainder(new BigInteger("10007")));
+	}
+
+}
