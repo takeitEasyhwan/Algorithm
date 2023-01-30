9f403196888badddf1898e02a1beb876d7d5f77b
Leejihwan <wlghks0508@naver.com>
2023-01-31 02:35:07 +0900

    Create Solution.java

---
+++ Programmers/문자열/영어 끝말잇기(12981)/Solution.java
@@ -0,0 +1,32 @@
+package baekjoon;
+
+import java.util.HashMap;
+
+class Solution {
+    public int[] solution(int n, String[] words) {
+        int[] answer = new int[2];
+        HashMap<Integer, String> hMap = new HashMap<Integer, String>();
+        int i=0;
+        for(i=0;i<words.length;i++) {
+        	
+        	if(hMap.containsValue(words[i])) {
+        		answer[0]=i%n+1;
+        		answer[1]=i/n+1;
+        		return answer;
+        	}
+        	else if(!hMap.isEmpty()) {
+        		if(hMap.get(i-1).charAt(hMap.get(i-1).length()-1)!=words[i].charAt(0)) {
+        			answer[0]=i%n+1;
+            		answer[1]=i/n+1;
+            		return answer;
+        		}
+        			
+        	}
+        	hMap.put(i, words[i]);
+        		
+        }
+        answer[0]=0;
+        answer[1]=0;
+        return answer;
+    }
+}
