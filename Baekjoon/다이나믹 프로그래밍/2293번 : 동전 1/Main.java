755dc0985decfc72b2743af6f7881e47b2138a88
Jihwan Lee <wlghks0508@naver.com>
2023-12-26 09:55:05 +0900

    Create Main.java

---
+++ Baekjoon/다이나믹 프로그래밍/2293번 : 동전 1/Main.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] coinArr = new int[n];
+        for(int i=0;i<n;i++) {
+            coinArr[i] = Integer.parseInt(br.readLine());
+        }
+        int[][] dp = new int[n+1][k+1];
+        dp[0][0]=1;
+        for(int i=1;i<n+1;i++) {
+            for(int j=0;j<k+1;j++) {
+                if(j<coinArr[i-1])
+                    dp[i][j] = dp[i-1][j];
+                else
+                    dp[i][j] = dp[i][j-coinArr[i-1]]+dp[i-1][j];
+            }
+        }
+        System.out.println(dp[n][k]);
+    }
+}
